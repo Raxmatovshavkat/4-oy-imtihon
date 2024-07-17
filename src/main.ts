baee7b40bcfebda75340a642d07c796cee285a1b
Shavkatboy <hamidrahmatov44@gmail.com>
2024-07-17 19:00:42 +0500

    hfbdj

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
   }));
-  await app.listen(3000);
+  const port = process.env.DATABASE_PORT
+  await app.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+    
+  });
 }
 bootstrap();
